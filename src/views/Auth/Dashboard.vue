019ad93d10c278bc15e17a58fbcb5e13eba069f2
Lascoyle <meursault45@live.com>
2021-03-29 13:52:50 +0200

    Add deletion feature on latest games added list, change some style on it

--- src/views/Auth/Dashboard.vue
+++ src/views/Auth/Dashboard.vue
@@ -1,12 +1,13 @@
 <template>
   <div id="profile" class="p-6 lg:p-8 lg:pt-20 h-full">
     <h1 class="profile-title text-4xl lg:pl-16 font-black mb-10 text-purple-900 lg:mt-10">Welcome {{ userProfile.username.charAt(0).toUpperCase() + userProfile.username.slice(1) }}!</h1>
-    <h2 class="ml-20">Here you can manage your collections, create new ones and add games to it!</h2>
+    <p class="ml-20">Here you can manage your collections, create new ones and add games to it!</p>
     <ul v-if="games.length != 0" class="p-20">
-        <li v-for="(game, index) in games" :key="index" class="flex w-full p-4 px-8 justify-between items-center bg-white border-">
+        <h2 class="recent-games-list-title text-purple-900 mb-5">Last games you've added</h2>
+        <li v-for="(game,index) in games" :key="index" class="flex w-full p-4 px-8 justify-between items-center bg-white border-l-8 hover:border-purple-600 hover:bg-gray-50">
           <div class="w-full flex justify-between items-center">
             <div class="w-2/12">
-              <router-link :to="{ name: 'Game', params: {id: game.api_id } }"><img :src="game.image" alt="" class="block h-24 "></router-link>
+              <router-link :to="{ name: 'Game', params: {id: game.api_id } }"><img :src="game.image" alt="" class="block h-36 "></router-link>
             </div>
             <details class="w-5/12">
               <summary class=" text-purple-900 game-listed-title cursor-pointer focus:outline-none">{{ game.title }}</summary>
@@ -27,7 +28,7 @@
                 <p v-if="releaseLabelShown === true" class="text-white bg-gray-700 text-center rounded-lg w-8/12 absolute left-12 p-1">Release</p>
               </transition>
             </div>
-            <button @click="deleteGame(game.title)"><img src="../../assets/icons/delete.png" alt="delete icon" class="w-6"></button>
+            <button @click="deleteGame(game.id)"><img src="../../assets/icons/delete.png" alt="delete icon" class="w-6"></button>
           </div>
         </li>
     </ul>
@@ -52,36 +53,35 @@ export default {
       ...mapState(['userProfile'])
     },
     created() {
-      fb.gamesCollection.where("user_id", "==", fb.auth.currentUser.uid).onSnapshot(snapshot => {
-        snapshot.forEach(doc => {
+      fb.gamesCollection.where("user_id", "==", fb.auth.currentUser.uid).orderBy('created_on', 'desc').limit(5).onSnapshot(snapshot => {
+        snapshot.docs.forEach(doc => {
           let game = doc.data();
+          game.id = doc.id;
           this.games.push(game);
+          console.log(this.games)
         })
       })
     },
+
     methods: {
-      deleteGame() {
-        fb.gamesCollection.doc().delete();
+      deleteGame(id) {
+        fb.gamesCollection.doc(id).delete();
         console.log('game deleted');
       },
 
       showPlatformLabel() {
-        console.log("shown")
         this.platformLabelShown = true;
       },
 
       unshowPlatformLabel() {
-        console.log("not shown")
         this.platformLabelShown = false;
       },
 
       showReleaseLabel() {
-        console.log("shown")
         this.releaseLabelShown = true;
       },
 
       unshowReleaseLabel() {
-        console.log("not shown")
         this.releaseLabelShown = false;
       }
     }
@@ -101,6 +101,11 @@ export default {
 
 .game-listed-title {
   font-family: "Audiowide", cursive;
+}
+
+.recent-games-list-title {
+  font-family: "Audiowide", cursive;
+  font-size: 3rem;
 }
 
 details[open] summary ~ * {
