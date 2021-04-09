c50ef856e711e8a6b9f6db4b4330237b333565be
Lascoyle <meursault45@live.com>
2021-04-09 13:46:05 +0200

    Change card background in Games view

--- src/views/Games/Games.vue
+++ src/views/Games/Games.vue
@@ -5,14 +5,14 @@
         <div class="next-games hover:text-purple-600 text-purple-400" @click="callNextPage()">❱</div>
         <div class="order-elmts flex justify-end items-center w-full mr-20 my-10">
             <div class="search ml-4 mr-12">
-                <input class="text-gray-600 rounded-full rounded-r-none border-r-0 border-4 focus:outline-none focus:bg-gray-900 focus:text-white transition duration-75 ease-linear p-2" type="text" placeholder="Search a game..." :value="queryGame" @keyup.enter="fetchGames">
-                <img src="../../assets/icons/search.svg" alt="" class="h-12 inline-block bg-white absolute border-4 border-l-0 rounded-full rounded-l-none p-2">
+                <input class="text-gray-600 rounded-full rounded-r-none border-r-0 border-4 border-purple-300 focus:outline-none focus:bg-gray-900 focus:text-white transition duration-75 ease-linear p-2" type="text" placeholder="Search a game..." :value="queryGame" @keyup.enter="fetchGames">
+                <img src="../../assets/icons/search.svg" alt="" class="h-12 inline-block bg-white absolute border-4 border-purple-300 border-l-0 rounded-full rounded-l-none p-2">
             </div>
             <div @click="sortByName()" class="order-elmt rounded-full transition duration-150 cursor-pointer hover:bg-gray-400 ease-linear bg-gray-900 font-bold p-3 px-4 ml-4 flex">
                 <img src="../../assets/icons/alphabetical.png" alt="" class="w-6 h-6 block mr-2">
                 Order by Name
             </div>
-            <div class=" order-elmt rounded-full transition duration-150 ease-linear cursor-pointer hover:bg-gray-400 bg-gray-900 font-bold mr-20 ml-4 p-3 px-4 flex" @click="fetchPlatforms()">
+            <div class="order-elmt rounded-full transition duration-150 ease-linear cursor-pointer hover:bg-gray-400 bg-gray-900 font-bold mr-20 ml-4 p-3 px-4 flex" @click="fetchPlatforms()">
                 <img src="../../assets/icons/console.png" alt="" class="w-6 h-6 block mr-2">
                 <p>Platforms</p>
             </div>
@@ -27,22 +27,22 @@
             </div>
         </transition>
         <ul class="games-list h-full flex flex-column justify-center lg:flex-row flex-wrap lg:px-16">
-            <li class="game card w-12/12 sm:w-6/12 lg:w-4/12 xl:w-3/12 p-4" v-for="(game, index) in games.results" :key="index">
+            <li class="game-card w-12/12 sm:w-6/12 lg:w-4/12 xl:w-3/12 p-4" v-for="(game, index) in games.results" :key="index">
                 <router-link :to="{ name: 'Game', params: { id: game.id } }">
                     <div class="game-image-container rounded-t-lg bg-gray-300">
                         <img v-if="game.background_image != null" :src="game.background_image" alt="" class="game-image block">
                         <img v-else src="../../assets/images/no-image.png" alt="" class="game-image block">
                     </div>
                 </router-link>
-                <div class="game-textual card-header bg-white rounded-b-lg">
-                    <header class="card-header   p-4">
+                <div class="game-textual card-header  rounded-b-lg">
+                    <header class="card-header p-4">
                         <div class="flex justify-between">
                             <router-link :to="{ name: 'Game', params: {id: game.id} }"><h2 class="game-name text-gray-500 text-2xl font-black w-10/12 mb-5">{{ game.name }}</h2></router-link>
                             <p class="game-rating"><span class="text-purple-500 font-bold text-3xl">{{ Math.round(game.rating) }}</span><span class="text-green-400 font-black text-xl">/{{ game.rating_top }}</span></p>
                         </div>
                         <div v-if="loggedIn" class="text-md text-gray-500 cursor-pointer "><span class="font-bold text-white bg-gray-500 px-2 rounded-full">✚</span> <span class="hover:underline"><router-link :to="{ name: 'GameAdd', params: {id: game.id}}">Add to collection</router-link></span></div>
                     </header>
-                   <div class="p-4">
+                   <div class="p-4 card-body ">
                         <ul class="genres-list flex flex-wrap justify-end font-semibold">
                                 <li class="genre text-sm text-green-400 p-1" v-for="(genre, index) in game.genres" :key="index">
                                     {{ genre.name }}
@@ -173,6 +173,13 @@ export default {
 
     .next-games {
         right: 2%;
+    }
+
+    .card-header {
+        background-color: #fefcff;
+        background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='100%25'%3E%3Cdefs%3E%3ClinearGradient id='a' gradientUnits='userSpaceOnUse' x1='0' x2='0' y1='0' y2='100%25' gradientTransform='rotate(284,768,375)'%3E%3Cstop offset='0' stop-color='%23fefcff'/%3E%3Cstop offset='1' stop-color='%23ffffff'/%3E%3C/linearGradient%3E%3Cpattern patternUnits='userSpaceOnUse' id='b' width='540' height='450' x='0' y='0' viewBox='0 0 1080 900'%3E%3Cg fill-opacity='0.02'%3E%3Cpolygon fill='%23444' points='90 150 0 300 180 300'/%3E%3Cpolygon points='90 150 180 0 0 0'/%3E%3Cpolygon fill='%23AAA' points='270 150 360 0 180 0'/%3E%3Cpolygon fill='%23DDD' points='450 150 360 300 540 300'/%3E%3Cpolygon fill='%23999' points='450 150 540 0 360 0'/%3E%3Cpolygon points='630 150 540 300 720 300'/%3E%3Cpolygon fill='%23DDD' points='630 150 720 0 540 0'/%3E%3Cpolygon fill='%23444' points='810 150 720 300 900 300'/%3E%3Cpolygon fill='%23FFF' points='810 150 900 0 720 0'/%3E%3Cpolygon fill='%23DDD' points='990 150 900 300 1080 300'/%3E%3Cpolygon fill='%23444' points='990 150 1080 0 900 0'/%3E%3Cpolygon fill='%23DDD' points='90 450 0 600 180 600'/%3E%3Cpolygon points='90 450 180 300 0 300'/%3E%3Cpolygon fill='%23666' points='270 450 180 600 360 600'/%3E%3Cpolygon fill='%23AAA' points='270 450 360 300 180 300'/%3E%3Cpolygon fill='%23DDD' points='450 450 360 600 540 600'/%3E%3Cpolygon fill='%23999' points='450 450 540 300 360 300'/%3E%3Cpolygon fill='%23999' points='630 450 540 600 720 600'/%3E%3Cpolygon fill='%23FFF' points='630 450 720 300 540 300'/%3E%3Cpolygon points='810 450 720 600 900 600'/%3E%3Cpolygon fill='%23DDD' points='810 450 900 300 720 300'/%3E%3Cpolygon fill='%23AAA' points='990 450 900 600 1080 600'/%3E%3Cpolygon fill='%23444' points='990 450 1080 300 900 300'/%3E%3Cpolygon fill='%23222' points='90 750 0 900 180 900'/%3E%3Cpolygon points='270 750 180 900 360 900'/%3E%3Cpolygon fill='%23DDD' points='270 750 360 600 180 600'/%3E%3Cpolygon points='450 750 540 600 360 600'/%3E%3Cpolygon points='630 750 540 900 720 900'/%3E%3Cpolygon fill='%23444' points='630 750 720 600 540 600'/%3E%3Cpolygon fill='%23AAA' points='810 750 720 900 900 900'/%3E%3Cpolygon fill='%23666' points='810 750 900 600 720 600'/%3E%3Cpolygon fill='%23999' points='990 750 900 900 1080 900'/%3E%3Cpolygon fill='%23999' points='180 0 90 150 270 150'/%3E%3Cpolygon fill='%23444' points='360 0 270 150 450 150'/%3E%3Cpolygon fill='%23FFF' points='540 0 450 150 630 150'/%3E%3Cpolygon points='900 0 810 150 990 150'/%3E%3Cpolygon fill='%23222' points='0 300 -90 450 90 450'/%3E%3Cpolygon fill='%23FFF' points='0 300 90 150 -90 150'/%3E%3Cpolygon fill='%23FFF' points='180 300 90 450 270 450'/%3E%3Cpolygon fill='%23666' points='180 300 270 150 90 150'/%3E%3Cpolygon fill='%23222' points='360 300 270 450 450 450'/%3E%3Cpolygon fill='%23FFF' points='360 300 450 150 270 150'/%3E%3Cpolygon fill='%23444' points='540 300 450 450 630 450'/%3E%3Cpolygon fill='%23222' points='540 300 630 150 450 150'/%3E%3Cpolygon fill='%23AAA' points='720 300 630 450 810 450'/%3E%3Cpolygon fill='%23666' points='720 300 810 150 630 150'/%3E%3Cpolygon fill='%23FFF' points='900 300 810 450 990 450'/%3E%3Cpolygon fill='%23999' points='900 300 990 150 810 150'/%3E%3Cpolygon points='0 600 -90 750 90 750'/%3E%3Cpolygon fill='%23666' points='0 600 90 450 -90 450'/%3E%3Cpolygon fill='%23AAA' points='180 600 90 750 270 750'/%3E%3Cpolygon fill='%23444' points='180 600 270 450 90 450'/%3E%3Cpolygon fill='%23444' points='360 600 270 750 450 750'/%3E%3Cpolygon fill='%23999' points='360 600 450 450 270 450'/%3E%3Cpolygon fill='%23666' points='540 600 630 450 450 450'/%3E%3Cpolygon fill='%23222' points='720 600 630 750 810 750'/%3E%3Cpolygon fill='%23FFF' points='900 600 810 750 990 750'/%3E%3Cpolygon fill='%23222' points='900 600 990 450 810 450'/%3E%3Cpolygon fill='%23DDD' points='0 900 90 750 -90 750'/%3E%3Cpolygon fill='%23444' points='180 900 270 750 90 750'/%3E%3Cpolygon fill='%23FFF' points='360 900 450 750 270 750'/%3E%3Cpolygon fill='%23AAA' points='540 900 630 750 450 750'/%3E%3Cpolygon fill='%23FFF' points='720 900 810 750 630 750'/%3E%3Cpolygon fill='%23222' points='900 900 990 750 810 750'/%3E%3Cpolygon fill='%23222' points='1080 300 990 450 1170 450'/%3E%3Cpolygon fill='%23FFF' points='1080 300 1170 150 990 150'/%3E%3Cpolygon points='1080 600 990 750 1170 750'/%3E%3Cpolygon fill='%23666' points='1080 600 1170 450 990 450'/%3E%3Cpolygon fill='%23DDD' points='1080 900 1170 750 990 750'/%3E%3C/g%3E%3C/pattern%3E%3C/defs%3E%3Crect x='0' y='0' fill='url(%23a)' width='100%25' height='100%25'/%3E%3Crect x='0' y='0' fill='url(%23b)' width='100%25' height='100%25'/%3E%3C/svg%3E");
+        background-attachment: fixed;
+        background-size: cover;
     }
 
     .game-image-container {
